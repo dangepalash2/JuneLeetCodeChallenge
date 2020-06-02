37e34a07270048d73b432f3be9eddda83ed73ad1
Palash Dange <dangepalash2@gmail.com>
2020-06-02 20:26:17 +0100

    Adding Day_2 Challenge Code

---
+++ Day_2_DeleteLinkedListNOde.java
@@ -0,0 +1,14 @@
+/** Problem - 237 Linked List
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
