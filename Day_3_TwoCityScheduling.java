ba8d8cb82aceebc39c3ab5b673cb544b3ac63ac3
Palash Dange <dangepalash2@gmail.com>
2020-06-03 21:27:11 +0100

    Added Day_3 TwoCostScheduling

--- Day_3_TwoCityScheduling.java
+++ Day_3_TwoCityScheduling.java
@@ -1,16 +1,21 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
+        //Creating new index with number N number of person
         int evenLength = costs.length/2;
         int[] temp = new int[evenLength * 2];
         int minimumCost = 0, j = 0;
+        //Iterating to calculate the difference
         for(int[] cost : costs){
             temp[j++] = cost[1] - cost[0];
             minimumCost += cost[0];
         }
+        //Sorting the calculated array -> temp
         Arrays.sort(temp);
         for(int i = 0; i < evenLength; i++){
+            //Add total cost minimumn
             minimumCost += temp[i];
         }
+        //return minimum cost
         return minimumCost;
     }
 }
